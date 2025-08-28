4b905aac51dc75a9a1032c94f88bbe63070101d9
Mahmoud Abdelhamied <Mahmoudabdelhamied68@gmail.com>
2025-08-28 11:14:47 -0700

    add  models/supplier.js

---
+++ models/supplier.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const supplierSchema = new mongoose.Schema({
+  name: { 
+      type: String,
+      required: [ true,"الاسم مطلوب"],
+      minLength:[5,'الاسم يجب ان يكون اكبر من خمسة حروف']
+   
+    },
+//   address: String,
+  phoneNumber: {
+    type: String,
+    required: [ true,"رقم الجوال مطلوب"],
+    match: [/^(009665|9665|\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/, "رقم الجوال غير صالح"]
+  },
+  funding: [{
+    type: String,
+    required: [ true,"جهات التمويل مطلوبة"]
+  }],
+  cars: [{ type: mongoose.Schema.Types.ObjectId, ref: "Car" }]
+});
+
+module.exports = mongoose.model("Supplier", supplierSchema);
