d802ce2ec59e035a75a7401b2186875bdb003313
Mahmoud Abdelhamied <Mahmoudabdelhamied68@gmail.com>
2025-09-06 10:41:37 -0700

    edite in index.js to show decu in vercel

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 // Swagger UI setup
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const userRoutes = require("./routes/user");
 app.use("/user", userRoutes);
